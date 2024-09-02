2c647e2cd4df11434e0504b5f4f61b94136dec16
Ken Brisco <kenbriscodev@gmail.com>
2024-09-02 10:04:15 -0500

    update

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: 'Next.js + NextUI',
-	description: 'Make beautiful websites regardless of your design experience.',
+	name: 'Incognito',
+	description: 'Empower Your Vision, Elevate Your Success.',
 	navItems: [
 		{
 			label: 'Home',
